35f8dab97d997e4feb7d7f9e9f24284b0afeb815
Naiizey <nairolfdu22@gmail.com>
2024-04-02 17:51:33 +0200

    ended slider's front

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,37 @@
 import { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './App.css';
 
 import Header from './components/header/header'
-import Slider from './components/slider/slider';
-import Home from './pages/home/home'
-import Story from './pages/story/story'
+import Main from './Main'
+
 
 function App() {
   const [isSliderOpened, setIsSliderOpened] = useState(false);
 
   const [sliderType, setSliderType] = useState();
 
+  /* useStates for future implementations
   const [darkMode, setDarkMode] = useState(true);
 
   const [displayMode, setDisplayMode] = useState("simple");
 
   const [lineSpace, setLineSpace] = useState(1);
 
+  Add this to Main & Slider :
+    darkMode={darkMode} setDarkMode={setDarkMode} 
+    displayMode={displayMode} setDisplayMode={setDisplayMode}
+    lineSpace={lineSpace} setLineSpace={setLineSpace}
+  */
+
   return (
     <Router>
       <Header isSliderOpened={isSliderOpened} setIsSliderOpened={setIsSliderOpened} sliderType={sliderType} setSliderType={setSliderType}/>
-      {isSliderOpened && 
-        <Slider 
-          sliderType={sliderType} setSliderType={setSliderType}
-          setIsSliderOpened={setIsSliderOpened} 
-          darkMode={darkMode} setDarkMode={setDarkMode} 
-          displayMode={displayMode} setDisplayMode={setDisplayMode}
-          lineSpace={lineSpace} setLineSpace={setLineSpace}
-        />
-      }
-      <Routes>
-        <Route
-          exact
-          path="/"
-          element={ <Home/> }
-        />
-        <Route
-          exact
-          path="/story"
-          element={ <Story/> }
-        />
-      </Routes>
+      <Main 
+        sliderType={sliderType} setSliderType={setSliderType}
+        isSliderOpened={isSliderOpened} setIsSliderOpened={setIsSliderOpened} 
+      />
     </Router>
   );
 }
