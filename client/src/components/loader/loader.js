a75cb993006d965f79aca7ffad55e562491cf2f4
Naiizey <nairolfdu22@gmail.com>
2024-04-03 23:12:57 +0200

    loader component done

---
+++ client/src/components/loader/loader.js
@@ -0,0 +1,16 @@
+import './loader.css'
+
+const Loader = () => {
+    return(
+        <div className="loaderComponent">
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </div>
+    )
+}
+
+export default Loader;
