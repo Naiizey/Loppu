bf492f7fddf604ccf4e295412e6138a3ee43d004
gablaxy <gab2003video@gmail.com>
2024-04-03 11:27:27 +0200

    Refactor characterSelection component and characterSheet styles

--- client/src/components/characterSheet/characterSheet.js
+++ client/src/components/characterSheet/characterSheet.js
@@ -2,42 +2,89 @@ import React from 'react';
 import './characterSheet.css';
 
 function click(name) {
-    console.log(name);
+    // console.log(name)
 }
 
-const characterSheet = ({img, name, stats, inventory}) => {
-    return (
-        <li id="characterSheet">
-            <img src={img} alt={name}/>
-            <h2>{name}</h2>
-            <ul>
-                <li>
-                    <p>Strength: </p>
-                    <p>{stats.strength}</p>
+const characterSheet = ({type, img, name, stats, inventory, isClicked, onClick}) => {
+    switch(type) {
+        case "big":
+            return (
+                <li id="characterSheetBig" className={`${isClicked ? "selected" : ""}`}   onClick={onClick}>
+                    <img src={img} alt={name}/>
+                    <h2>{name}</h2>
+                    <ul>
+                        <li>
+                            <p>Strength </p>
+                            <p>{stats.strength}</p>
+                        </li>
+                        <li>
+                            <p>Intelligence </p>
+                            <p>{stats.intelligence}</p>
+                        </li>
+                        <li>
+                            <p>Resistance </p>
+                            <p>{stats.resistance}</p>
+                        </li>
+                        <li>
+                            <p>Luck </p>
+                            <p>{stats.luck}</p>
+                        </li>
+                    </ul>
+                    <hr/>
+                    <p>Inventory </p>
+                    <ul>
+                        {inventory.map((item, index) => {
+                            return <li key={index}>{item}</li>
+                        })}
+                    </ul>
+                    <button onClick={click(name)}>Choisir</button>
                 </li>
-                <li>
-                    <p>Intelligence: </p>
-                    <p>{stats.intelligence}</p>
+            )
+        case "small":
+            return (
+                <li id="characterSheetSmall" className={`${isClicked ? "selected" : ""}`}   onClick={onClick}>
+                    <img src={img} alt={name}/>
+                    <section>
+                        <h2>{name}</h2>
+                        <ul>
+                            <li>
+                                <p>Strength </p>
+                                <p>{stats.strength}</p>
+                            </li>
+                            <li>
+                                <p>Intelligence </p>
+                                <p>{stats.intelligence}</p>
+                            </li>
+                            <li>
+                                <p>Resistance </p>
+                                <p>{stats.resistance}</p>
+                            </li>
+                            <li>
+                                <p>Luck </p>
+                                <p>{stats.luck}</p>
+                            </li>
+                        </ul>
+                    </section>
+                    { isClicked &&
+                        <>
+                            <hr/>
+                            <p>Inventory </p>
+                            <ul>
+                                {inventory.map((item, index) => {
+                                    return <li key={index}>{item}</li>
+                                })}
+                            </ul>
+                        </>
+                    }
                 </li>
-                <li>
-                    <p>Resistance: </p>
-                    <p>{stats.resistance}</p>
+            )
+        default:
+            return (
+                <li id="characterSheet">
+                    default case
                 </li>
-                <li>
-                    <p>Luck: </p>
-                    <p>{stats.luck}</p>
-                </li>
-            </ul>
-            <hr/>
-            <p>Inventory:</p>
-            <ul>
-                {inventory.map((item, index) => {
-                    return <li key={index}>{item}</li>
-                })}
-            </ul>
-            <button onClick={click(name)}>Choisir</button>
-        </li>
-    )
+            )
+    }
 };
 
 export default characterSheet;
--- client/src/pages/characterSelection/characterSelection.js
+++ client/src/pages/characterSelection/characterSelection.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import { useState } from 'react';
 import './characterSelection.css';
 import CharacterSheet from '../../components/characterSheet/characterSheet';
 
-const characterSelection = () => {
+const CharacterSelection = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
     return (
         <section id="characterSelection">
             <h1>Choose your character</h1>
             <ul>
-                <CharacterSheet name="Warrior" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["sword", "shield"]} img="https://via.placeholder.com/150"/>
-                <CharacterSheet name="Mage" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["staff", "spellbook"]} img="https://via.placeholder.com/150"/>
-                <CharacterSheet name="Rogue" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["dagger", "lockpick"]} img="https://via.placeholder.com/150"/>
+                <CharacterSheet type="big" name="Warrior" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["sword", "shield"]} img="https://via.placeholder.com/150"/>
+                <CharacterSheet type="small" name="Mage" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["staff", "spellbook"]} img="https://via.placeholder.com/150" isClicked={isClicked} onClick={() => setIsClicked(!isClicked)}/>
+                {/*
+                    <CharacterSheet type="big" name="Rogue" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["dagger", "lockpick"]} img="https://via.placeholder.com/150"/>
+                */}
             </ul>
         </section>
     )
 };
 
-export default characterSelection;
+export default CharacterSelection;
