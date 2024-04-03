029ac1c5fce1cb60f088b57fe051a90fa73db1c8
Naiizey <nairolfdu22@gmail.com>
2024-04-03 10:50:07 +0200

    storiesDisplay beginning

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,10 +1,23 @@
 import './home.css';
+import StoriesDisplay from '../../components/storiesDisplay/storiesDisplay';
+import Image from '../../assets/images/storiesDisplay.jpg'
+import { useState } from 'react';
+
 
 const HomePage = () => {
+    const [isClicked, setIsClicked] = useState(false)
+
     return (
         <main id="home">
-            <h1>Welcome back to Loppu !</h1>
-            <h2>My stories</h2>
+            <section className="welcomeMessage">
+                <h1>Welcome back to Loppu !</h1>
+                <h2>My stories</h2>
+            </section>
+            <section className="storiesList">
+                <StoriesDisplay Image={Image} name="Distortion of Ascalon" isStarted={true} isClicked={true} onClick={(e) => (console.log(e))}/>
+                <StoriesDisplay Image={Image} name="Distortion of Ascalon" isStarted={true} isClicked={false}/>
+                <StoriesDisplay Image={Image} name="Distortion of Ascalon" isStarted={true} isClicked={true} onClick={(e) => (console.log(e))}/>
+            </section>
         </main>
     )
 };
