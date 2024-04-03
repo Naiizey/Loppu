38c4d95692fefe1510b132f08bfd407b15e3cb1b
az4aaz_ <lucienherve@hotmail.com>
2024-04-03 23:37:56 +0200

    ptit changement

--- client/src/components/choices/choices.js
+++ client/src/components/choices/choices.js
@@ -6,45 +6,46 @@ import { useEffect, useState } from "react";
 import API from "../../utils/API";
 
 const Choices = ({ id, setSectionId }) => {
-    const [choices, setChoices] = useState([
-        {
-            content: "",
-            id_section_from: 0,
-            id_section_to : 0,
-            id_story: 0,
-            impact : {},
-            lose : false,
-            parent_key : "",
-            victory : false,
-        }
-    ]);
-    const story_id = localStorage.getItem("storyId");
+  const [choices, setChoices] = useState([
+    {
+      content: "",
+      id_section_from: 0,
+      id_section_to: 0,
+      id_story: 0,
+      impact: {},
+      lose: false,
+      parent_key: "",
+      victory: false,
+    },
+  ]);
+  const story_id = localStorage.getItem("storyId");
 
-    useEffect(() => {
-        API("choices/" + story_id + "/" + id).then((res) => {
-            res = res;
-            console.log(res);
-            setChoices(res);
-        });
-    }, [id]);
+  useEffect(() => {
+    API("choices/" + story_id + "/" + id).then((res) => {
+      res = res;
+      console.log(res);
+      setChoices(res);
+    });
+  }, [id]);
 
-    return (
-        <div>
-            {
-                choices.map((item, i) => {
-                    return (
-                        <Button
-                            key={i}
-                            size={"small"}
-                            type={"info"}
-                            text={item.content}
-                            onClick={() => { setChoices(item.id_section_to); setSectionId(item.id_section_to) }}
-                        />
-                    )
-                })
-            }
-        </div>
-    );
+  return (
+    <div>
+      {choices.map((item, i) => {
+        return (
+          <Button
+            key={i}
+            size={"small"}
+            type={"info"}
+            text={item.content}
+            onClick={() => {
+              setChoices(item.id_section_to);
+              setSectionId(item.id_section_to);
+            }}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Choices;
