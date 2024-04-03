2b3d30cfd0063480e489b684f5608bdcb45f778e
gablaxy <gab2003video@gmail.com>
2024-04-03 11:40:14 +0200

    Refactor characterSheet component styles and add expand/collapse functionality

--- client/src/components/characterSheet/characterSheet.js
+++ client/src/components/characterSheet/characterSheet.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import './characterSheet.css';
 
-function click(name) {
-    // console.log(name)
-}
-
 const characterSheet = ({type, img, name, stats, inventory, isClicked, onClick}) => {
     switch(type) {
         case "big":
             return (
                 <li id="characterSheetBig" className={`${isClicked ? "selected" : ""}`}   onClick={onClick}>
                     <img src={img} alt={name}/>
-                    <h2>{name}</h2>
-                    <ul>
-                        <li>
-                            <p>Strength </p>
-                            <p>{stats.strength}</p>
-                        </li>
-                        <li>
-                            <p>Intelligence </p>
-                            <p>{stats.intelligence}</p>
-                        </li>
-                        <li>
-                            <p>Resistance </p>
-                            <p>{stats.resistance}</p>
-                        </li>
-                        <li>
-                            <p>Luck </p>
-                            <p>{stats.luck}</p>
-                        </li>
-                    </ul>
-                    <hr/>
-                    <p>Inventory </p>
-                    <ul>
-                        {inventory.map((item, index) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                    </ul>
-                    <button onClick={click(name)}>Choisir</button>
+                    <section>
+                        <h2>{name}</h2>
+                        <ul>
+                            <li>
+                                <p>Strength </p>
+                                <p>{stats.strength}</p>
+                            </li>
+                            <li>
+                                <p>Intelligence </p>
+                                <p>{stats.intelligence}</p>
+                            </li>
+                            <li>
+                                <p>Resistance </p>
+                                <p>{stats.resistance}</p>
+                            </li>
+                            <li>
+                                <p>Luck </p>
+                                <p>{stats.luck}</p>
+                            </li>
+                        </ul>
+                    </section>
+                    <div id="arrowDownWhite">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 15.5l-6.5-6.5 1.5-1.5 5 5 5-5 1.5 1.5z"/></svg>
+                    </div>
+                    { isClicked &&
+                        <article>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo
+                            </p>
+                            <button>Choose</button>
+                        </article>
+                    }
                 </li>
             )
         case "small":
--- client/src/pages/characterSelection/characterSelection.js
+++ client/src/pages/characterSelection/characterSelection.js
@@ -9,7 +9,7 @@ const CharacterSelection = () => {
         <section id="characterSelection">
             <h1>Choose your character</h1>
             <ul>
-                <CharacterSheet type="big" name="Warrior" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["sword", "shield"]} img="https://via.placeholder.com/150"/>
+                <CharacterSheet type="big" name="Warrior" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["sword", "shield"]} img="https://via.placeholder.com/150" isClicked={isClicked} onClick={() => setIsClicked(!isClicked)}/>
                 <CharacterSheet type="small" name="Mage" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["staff", "spellbook"]} img="https://via.placeholder.com/150" isClicked={isClicked} onClick={() => setIsClicked(!isClicked)}/>
                 {/*
                     <CharacterSheet type="big" name="Rogue" stats={{strength:"10", intelligence:"5", resistance:"8", luck:"3"}} inventory={["dagger", "lockpick"]} img="https://via.placeholder.com/150"/>
