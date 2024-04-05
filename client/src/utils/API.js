01b5e493323a5337cafe68b57f835f323ab45535
gablaxy <32848524+gablaxy@users.noreply.github.com>
2024-04-05 11:04:54 +0200

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,14 @@
-// API call to get or set data from the database
+/**
+ * @function API
+ * 
+ * @param {string} content
+ * @param {string} method
+ * @param {object} json
+ * 
+ * @returns {Promise}
+ * 
+ * @description This function takes a string content, a string method, and an object json as parameters and makes an API call to get or set data from the database.
+ */
 const API = (content, method = "GET", json = {}) => {
   if (method === "GET") {
     return fetch(`http://localhost:5001/api/${content}`).then((res) =>
