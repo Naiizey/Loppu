d71c946ec6c7c81cb5489b38c258529ac3f8525c
Naiizey <nairolfdu22@gmail.com>
2024-06-03 16:09:01 +0200

    fix hp bar displaying an extra hitpoint

--- client/src/pages/story/story.js
+++ client/src/pages/story/story.js
@@ -251,7 +251,7 @@ const SectionPage = () => {
               <div className="hpBar">
                 {
                   Array.from({length: maxEnemyHealth}).map((hp, index) => (
-                    <div ref={hp} className={`${index <= currEnemyHealth ? 'hitpoint currHealth' : 'hitpoint'}`}></div>
+                    <div ref={hp} className={`${index + 1 <= currEnemyHealth ? 'hitpoint currHealth' : 'hitpoint'}`}></div>
                   ))
                 }
               </div>
